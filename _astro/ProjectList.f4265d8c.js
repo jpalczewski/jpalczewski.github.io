734441a5642cf3a1a341d3c82e0e8f71e50ccb54
jpalczewski <actions@github.com>
2023-09-06 21:22:37 +0000

    Update from https://github.com/jpalczewski/entuzjazm/commit/f3f454d317ded20d764a3f0ebc940570f58eae52

--- _astro/ProjectList.f4265d8c.js
+++ _astro/ProjectList.f4265d8c.js
@@ -1,1 +1,1 @@
-import{P as p}from"./ProjectList.645df4b6.js";import"./index.7db25182.js";/* empty css                          */export{p as default};
+import{P as p}from"./ProjectList.645df4b6.js";import"./index.7db25182.js";export{p as default};
