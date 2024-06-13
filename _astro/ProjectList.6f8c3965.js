a8711b970d0884dc4ad7397ad4c8368837b9a781
jpalczewski <actions@github.com>
2024-06-13 12:43:22 +0000

    Update from https://github.com/jpalczewski/entuzjazm-quartz/commit/c9e809b903af578a6e467c21290206e0bd62a459

--- _astro/ProjectList.6f8c3965.js
+++
@@ -1,1 +0,0 @@
-import{P as i}from"./ProjectList.38068c35.js";import"./index.aa289c0f.js";import"./astro-assets-services.9bc8d607.js";export{i as default};
--- _astro/post-1.502566ef.js
+++
@@ -1,9 +0,0 @@
-const a="pl/post-1.md",n="insight",t="pl/post-1",e=`
-Ze względu na to że pisanie mi zaczęło sprawiać mnóstwo frajdy[^1] - wracam ponownie do blogowania.[^2]
-
-[^1]: Ale pisanie po grupach na FB jest dość efemeryczne - fajnie jest jak niektóre rzeczy zostają na dłużej.
-[^2]: Za dużo zachwalania blogowania na HN też miało swój skutek
-`,o={title:"Witaj, astro!",language:"pl",date:new Date(1691504623e3)},i={type:"content",filePath:"/home/runner/work/entuzjazm/entuzjazm/src/content/insight/pl/post-1.md",rawData:`
-title: "Witaj, astro!"
-language: pl
-date: "2023-08-08T12:23:43-02:00"`};export{i as _internal,e as body,n as collection,o as data,a as id,t as slug};
