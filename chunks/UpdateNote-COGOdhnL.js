56015952eea66d8014685a2776406795dac902d9
jpalczewski <jpalczewski@gmail.com>
2026-04-19 13:30:16 +0000

    deploy: karpaty@88243ed9f0c75bfc5e187bdf8489b9a78da1cb14

--- chunks/UpdateNote-COGOdhnL.js
+++ chunks/UpdateNote-COGOdhnL.js
@@ -1,5 +1,5 @@
 import { e as escape_html, a as ensure_array_like, c as attr, f as derived, d as attr_style, s as stringify } from './_at-astro-renderers-BLtlkU0u.js';
-import { g as getLocale } from './Layout-Dp_qGLOy.js';
+import { g as getLocale } from './Layout-BOLQ8vN3.js';
 /* empty css                         */
 
 /* eslint-disable */
