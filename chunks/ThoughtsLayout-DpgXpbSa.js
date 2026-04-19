ebccea1040f2137a599b3b974ee6b96e231f2c5d
jpalczewski <jpalczewski@gmail.com>
2026-04-19 15:53:41 +0000

    deploy: karpaty@4630240805c8732eca4373a24e99e4ce47c1ef56

--- chunks/ThoughtsLayout-DpgXpbSa.js
+++ chunks/ThoughtsLayout-DpgXpbSa.js
@@ -1,7 +1,7 @@
 import { c as createAstro, a as createComponent, r as renderComponent, b as renderTemplate, m as maybeRenderHead, e as renderSlot } from './astro-server-CNTtBI_2.js';
 import 'piccolore';
-import { $ as $$Layout } from './Layout-CFEyZRCB.js';
-import { g as getCollection, i as getPostsForLocale, A as getUniqueYears, B as getUniqueYearMonths } from './content-D7VCeSST.js';
+import { $ as $$Layout } from './Layout-DNYmeO47.js';
+import { g as getCollection, i as getPostsForLocale, A as getUniqueYears, B as getUniqueYearMonths } from './content-B_PdEcE1.js';
 import { a as createThoughtsNavConfig } from './secondary-nav-config-0UbuDUCn.js';
 
 const $$Astro = createAstro("https://blog.palczew.ski");
